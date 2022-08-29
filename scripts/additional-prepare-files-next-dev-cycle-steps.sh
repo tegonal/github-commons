fbe4beccb792685542701bc149e94cbcee6ca109
Robert Stoll <robert.stoll@tegonal.com>
2022-08-29 13:44:22 +0200

    v0.1.0

--- scripts/additional-prepare-files-next-dev-cycle-steps.sh
+++ scripts/additional-prepare-files-next-dev-cycle-steps.sh
@@ -1,1 +1,38 @@
-/home/tegonal/projects/ght/github-commons/scripts/additional-release-files-preparations.sh
+#!/usr/bin/env bash
+#
+#    __                          __
+#   / /____ ___ ____  ___  ___ _/ /       This script is provided to you by https://github.com/tegonal/github-commons
+#  / __/ -_) _ `/ _ \/ _ \/ _ `/ /        It is licensed under Creative Commons Zero v1.0 Universal
+#  \__/\__/\_, /\___/_//_/\_,_/_/         Please report bugs and contribute back your improvements
+#         /___/
+#                                         Version: v0.1.0
+#
+###################################
+set -euo pipefail
+shopt -s inherit_errexit
+unset CDPATH
+
+if ! [[ -v scriptsDir ]]; then
+	scriptsDir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" >/dev/null && pwd 2>/dev/null)"
+	declare -r scriptsDir
+fi
+
+if ! [[ -v projectDir ]]; then
+	projectDir="$(realpath "$scriptsDir/../")"
+	declare -r projectDir
+fi
+
+if ! [[ -v dir_of_tegonal_scripts ]]; then
+	dir_of_tegonal_scripts="$projectDir/lib/tegonal-scripts/src"
+	source "$dir_of_tegonal_scripts/setup.sh" "$dir_of_tegonal_scripts"
+fi
+
+if ! [[ -v version ]]; then
+	die "looks like \$version was not defined by release-files.sh where this file is supposed to be sourced."
+fi
+
+find "$projectDir/src" -type f \
+	-not -name "*.sh" -print0 |
+	while read -r -d $'\0' file; do
+		perl -0777 -i -pe "s/(# {4,}Version: ).*/\${1}$version/g;" "$file"
+	done
