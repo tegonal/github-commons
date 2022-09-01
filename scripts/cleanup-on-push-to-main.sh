a52c4f0ffbce42c96b2a85ba6a2387cfb09f375a
Robert Stoll <robert.stoll@tegonal.com>
2022-09-01 22:31:58 +0200

    v0.4.3

--- scripts/cleanup-on-push-to-main.sh
+++ scripts/cleanup-on-push-to-main.sh
@@ -5,13 +5,13 @@
 #  / __/ -_) _ `/ _ \/ _ \/ _ `/ /        It is licensed under Creative Commons Zero v1.0 Universal
 #  \__/\__/\_, /\___/_//_/\_,_/_/         Please report bugs and contribute back your improvements
 #         /___/
-#                                         Version: v0.5.0-SNAPSHOT
+#                                         Version: v0.4.3
 #
 ###################################
 set -euo pipefail
 shopt -s inherit_errexit
 unset CDPATH
-TEGONAL_GITHUB_COMMONS_LATEST_VERSION="v0.4.2"
+TEGONAL_GITHUB_COMMONS_LATEST_VERSION="v0.4.3"
 
 if ! [[ -v scriptsDir ]]; then
 	scriptsDir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" >/dev/null && pwd 2>/dev/null)"
