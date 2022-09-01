81f5dadc8a5d653a443bed08870e9d58a1df0b12
Robert Stoll <robert.stoll@tegonal.com>
2022-09-01 09:26:05 +0200

    add a docker-entrypoint.sh which can be re-used to exec arbitrary code

---
+++ src/ci/docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+#    __                          __
+#   / /____ ___ ____  ___  ___ _/ /       This script is provided to you by https://github.com/tegonal/github-commons
+#  / __/ -_) _ `/ _ \/ _ \/ _ `/ /        It is licensed under Creative Commons Zero v1.0 Universal
+#  \__/\__/\_, /\___/_//_/\_,_/_/         Please report bugs and contribute back your improvements
+#         /___/
+#                                         Version: v0.4.0-SNAPSHOT
+#
+###################################
+set -euo pipefail
+shopt -s inherit_errexit
+unset CDPATH
+
+if ! (($# == 1)); then
+	echo >&2 "no commands provided via first argument or more than one argument provided"
+	exit 1
+fi
+
+# e.g. gitlab passes `sh -c ...`, in this case we just exec bash and the command will then be executed there
+if [[ "$1" =~ ^sh|bash ]]; then
+	/usr/bin/env bash
+else
+	eval "$1"
+fi
--- src/gget/pull-hook-functions.sh
+++ src/gget/pull-hook-functions.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #
 #    __                          __
-#   / /____ ___ ____  ___  ___ _/ /       This script is provided to you by https://github.com/tegonal/gget
-#  / __/ -_) _ `/ _ \/ _ \/ _ `/ /        It is licensed under Apache 2.0
+#   / /____ ___ ____  ___  ___ _/ /       This script is provided to you by https://github.com/tegonal/github-commons
+#  / __/ -_) _ `/ _ \/ _ \/ _ `/ /        It is licensed under Creative Commons Zero v1.0 Universal
 #  \__/\__/\_, /\___/_//_/\_,_/_/         Please report bugs and contribute back your improvements
 #         /___/
 #                                         Version: v0.4.0-SNAPSHOT
